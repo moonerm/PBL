f8dcc9c7659b9abfe3ea49cb32d14f11c034f9fb
moonerm <forvictori01@gmail.com>
2018-11-19 21:13:26 +0900

    Add horizontal chart & modifying bar chart in homefragment

--- app/src/main/java/com/test/pbl/mypagefragment.java
+++ app/src/main/java/com/test/pbl/mypagefragment.java
@@ -7,8 +7,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.test.pbl.homefragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.data.BarData;
@@ -20,14 +22,43 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class mypagefragment extends Fragment {
     @Nullable
     private BarChart mChart;
     private LineChart lineChart;
+    private HorizontalBarChart horizontalBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mypagefragment, container, false);
+
+        //수평막대그래프입니다
+        horizontalBar = (HorizontalBarChart) view.findViewById(R.id.horizontalChart);
+
+        horizontalBar.setNoDataText("");
+        horizontalBar.setDrawBarShadow(true);   //얘는 목표량이기 때문에 shadow 그려줍시다
+        horizontalBar.getAxisLeft().setAxisMaximum(50f);  //최대값은 사용자 지정이야? 그거 아니면 걍 50하자
+        horizontalBar.getAxisLeft().setAxisMinimum(0f);
+        horizontalBar.getXAxis().setAxisMinimum(0f); //그래프 0부터 시작합시다
+        horizontalBar.getAxisRight().setDrawGridLines(false);
+        horizontalBar.getAxisLeft().setDrawGridLines(false);
+        horizontalBar.getXAxis().setDrawGridLines(false); //grid 삭제
+        horizontalBar.setScaleEnabled(false);
+        horizontalBar.getXAxis().setEnabled(false);
+        horizontalBar.getAxisRight().setEnabled(false);
+        horizontalBar.getDescription().setEnabled(false); //오른쪽 코너에 성가신 text 치우기
+        horizontalBar.getLegend().setEnabled(false); //그래프 아래에 성가신 data set 정보도 치우기
+
+        ArrayList<BarEntry> horizontalVal = new ArrayList<>();
+
+        horizontalVal.add(new BarEntry(0,25));
+        BarDataSet horizontalSet = new BarDataSet(horizontalVal, "Horizontal Bar Data Set");
+        horizontalSet.setColors(Color.parseColor("#FAD064"));
+        horizontalSet.setDrawValues(false);  //사실 bar 옆에 value가 떴음 좋겠는데 이상하게 빗나가서 보류해둔다,,
+        BarData horizontalData = new BarData(horizontalSet);
+        horizontalBar.setData(horizontalData);
 
         //막대그래프입니다
         mChart = (BarChart) view.findViewById(R.id.myChart);
@@ -47,6 +78,7 @@ public class mypagefragment extends Fragment {
 
         ArrayList<BarEntry> barEntries = new ArrayList<>();
 
+
         //여기는 데이터를 받아 올 자리
         barEntries.add(new BarEntry(1, 40f));
         barEntries.add(new BarEntry(2, 30f));
@@ -58,9 +90,11 @@ public class mypagefragment extends Fragment {
 
         AxisBase axis;
 
+        Calendar cal = Calendar.getInstance();
+        int dayNum = cal.get(Calendar.DAY_OF_WEEK);
         BarDataSet dataSet = new BarDataSet(barEntries, "Chart Data Set");
         dataSet.setDrawValues(false); //그래프 위에 데이터가 안 뜨게 함다
-        dataSet.setColors(Color.parseColor("#FAD064"));
+        dataSet.setColors(Color.LTGRAY);
         BarData chartData = new BarData(dataSet);
         mChart.setData(chartData);
 
@@ -83,6 +117,7 @@ public class mypagefragment extends Fragment {
         lineChart.fitScreen();
         lineChart.getXAxis().setSpaceMax(0.5f);
         lineChart.getXAxis().setSpaceMin(0.5f);
+        lineChart.getAxisLeft().setAxisMinimum(0f);  //얘도 그래프 0부터 시작합시다
 
 
         ArrayList<Entry> lineEntries = new ArrayList<>();
