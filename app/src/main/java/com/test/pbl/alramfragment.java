276a016f62a0bf1cd45ecbfe310ae5cf92910736
kkulhotteok <maboros7@gmail.com>
2018-11-20 20:46:45 +0900

    #Alram #shareprefernce 미적용

--- app/src/main/java/com/test/pbl/alramfragment.java
+++ app/src/main/java/com/test/pbl/alramfragment.java
@@ -2,25 +2,60 @@ package com.test.pbl;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
-public class alramfragment extends Fragment implements MainActivity.OnBackPressedListener{
+public class alramfragment extends PreferenceFragment implements MainActivity.OnBackPressedListener{
+    private SharedPreferences alram_Pref;
+    private SharedPreferences.Editor mPrefEdit;
+
     homefragment mainFragment;
+    Button button3;
+    Switch alarmck;
+    Toast testToast;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.alramfragment, container, false);
+        final View view = inflater.inflate(R.layout.alramfragment, container, false);
         mainFragment = new homefragment();
-        return view;
 
+        alarmck = (Switch)view.findViewById(R.id.alarmck);
+
+        alarmck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    TimePickerFragment timePickerFragment = new TimePickerFragment();
+                    timePickerFragment.show(getFragmentManager(), "timePicker");
+                    alarmck.setChecked(true);
+                }
+                else{
+                }
+            }
+        });
+
+
+        return view;
     }
+
+//    public void showAlarmDialog(View view) {
+//        this.getView();
+//        TimePickerFragment timePickerFragment = new TimePickerFragment();
+//        timePickerFragment.show(getFragmentManager(), "timePicker");
+//    }
 
     @Override
     public void onBack() {
